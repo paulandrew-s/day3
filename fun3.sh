880ec3fd06eda8b90d4ed55ee5a24c82ce731698
Paul Andrew S <paulandrew.s@prodapt.com>
2026-07-08 10:30:53 +0530

    completed fun3

--- fun3.sh
+++ fun3.sh
@@ -9,3 +9,15 @@ fi
 echo "Enter a number"
 read n
 check_odd_even $n
+rev_str=""
+# String reversal
+reverse_string(){
+str=$1
+for (( i=${#str}-1; i >= 0; i-- ))
+do
+	rev_str=${rev_str}${str:$i:1}
+done
+echo $rev_str
+}
+
+reverse_string Linux
